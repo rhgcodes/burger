72df80f624aae9cd39488c07ff9fa4cbf12e7018
rhgcodes <rgalleguillos87@outlook.com>
2019-01-11 11:44:11 -0500

    final

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Megaburger');
+INSERT INTO burgers (burger_name) VALUES ('Superburger');
+INSERT INTO burgers (burger_name) VALUES ('Ultraburger');
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
+
 var PORT = process.env.PORT || 8080;
 var app = express();
 
+var methodOverride = require('method-override');
 
+app.use(methodOverride('_method'));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
