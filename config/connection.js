6eb1a83469356dd1276c2decaebe493e043b0255
rhgcodes <rgalleguillos87@outlook.com>
2019-01-24 21:07:34 -0500

    updated heroku

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,16 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-connection.connect();
-
-// connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-//   if (err) throw err;
-
-//   console.log('The solution is: ', rows[0].solution);
-// });
+var mysql = require("mysql");
+var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
- } else {
-  connection = mysql.createConnection({
-    host: 'wftuqljwesiffol6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'l3gw3auder2h10ak',
-    password: 'auze9fo2gs8jrbi1',
-    database: 'ive1gu4tzulbdl1q'
-  })
- };
+} else {
+    connection = mysql.createConnection({
+    host: "wftuqljwesiffol6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "l3gw3auder2h10ak",
+    password: "auze9fo2gs8jrbi1",
+    database: "ive1gu4tzulbdl1q"
+  });
+}
 
 connection.end();
 
