25e1b86850ac4df50befc4196610d1108c650d8c
rhgcodes <rgalleguillos87@outlook.com>
2019-01-24 21:28:03 -0500

    finally i hope

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,20 @@ if (process.env.JAWSDB_URL) {
 } else {
     connection = mysql.createConnection({
     host: "localhost",
+    port: 3306,
     user: "root",
     password: "root",
-    database: "burgers"
+    database: "burgers_db"
   });
 }
 
-connection.end();
-
-module.exports = connection;
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+ });
+ 
+ // Export connection for our ORM to use.
+ module.exports = connection;
